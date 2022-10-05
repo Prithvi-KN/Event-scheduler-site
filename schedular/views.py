84e2c7a4db5b33351b839a58d74a26e02372d933
Prithvi-KN <115085242+Prithvi-KN@users.noreply.github.com>
2022-10-05 08:48:09 -0400

    Add files via upload

---
+++ schedular/views.py
@@ -0,0 +1,152 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def chooseEvents(requests):
+    return render(requests, 'chooseEvents.html')
+
+def chooseLocationEdu(request):
+    return render(request, 'chooseLocationEdu.html')
+
+def chooseLocationSpo(request):
+    return render(request, 'chooseLocationSpo.html')
+
+def chooseLocationCha(request):
+    return render(request, 'chooseLocationCha.html')
+
+def chooseLocationCul(request):
+    return render(request, 'chooseLocationCul.html')
+
+def EduBang(request):
+    return render(request, 'EduBang.html')
+
+def EduChen(request):
+    return render(request, 'EduChen.html')
+
+def EduKol(request):
+    return render(request, 'EduKol.html')
+
+def EduMumb(request):
+    return render(request, 'EduMumb.html')
+
+def EduAny(request):
+    return render(request, 'EduAny.html')
+
+def SpoBang(request):
+    return render(request, 'SpoBang.html')
+
+def SpoChen(request):
+    return render(request, 'SpoChen.html')
+
+def SpoKol(request):
+    return render(request, 'SpoKol.html')
+
+def SpoMumb(request):
+    return render(request, 'SpoMumb.html')
+
+def SpoAny(request):
+    return render(request, 'SpoAny.html')
+
+def ChaBang(request):
+    return render(request, 'ChaBang.html')
+
+def ChaChen(request):
+    return render(request, 'ChaChen.html')
+
+def ChaKol(request):
+    return render(request, 'ChaKol.html')
+
+def ChaMumb(request):
+    return render(request, 'ChaMumb.html')
+
+def ChaAny(request):
+    return render(request, 'ChaAny.html')
+
+def CulBang(request):
+    return render(request, 'CulBang.html')
+
+def CulChen(request):
+    return render(request, 'CulChen.html')
+
+def CulKol(request):
+    return render(request, 'CulKol.html')
+
+def CulMumb(request):
+    return render(request, 'CulMumb.html')
+
+def CulAny(request):
+    return render(request, 'CulAny.html')
+
+
+def EduBangDetails1(request):
+    return render(request, 'EduBangDetails1.html')
+
+def EduBangDetails2(request):
+    return render(request, 'EduBangDetails2.html')
+
+def EduChenDetails1(request):
+    return render(request, 'EduChenDetails1.html')
+
+def EduChenDetails2(request):
+    return render(request, 'EduChenDetails2.html')
+
+def EduKolDetails(request):
+    return render(request, 'EduKolDetails.html')
+
+def EduMumbDetails(request):
+    return render(request, 'EduMumbDetails.html')
+
+def SpoBangDetails1(request):
+    return render(request, 'SpoBangDetails1.html')
+
+def SpoBangDetails2(request):
+    return render(request, 'SpoBangDetails2.html')
+
+def SpoChenDetails1(request):
+    return render(request, 'SpoChenDetails1.html')
+
+def SpoChenDetails2(request):
+    return render(request, 'SpoChenDetails2.html')
+
+def SpoKolDetails(request):
+    return render(request, 'SpoKolDetails.html')
+
+def SpoMumbDetails(request):
+    return render(request, 'SpoMumbDetails.html')
+
+def ChaBangDetails1(request):
+    return render(request, 'ChaBangDetails1.html')
+
+def ChaBangDetails2(request):
+    return render(request, 'ChaBangDetails2.html')
+
+def ChaChenDetails(request):
+    return render(request, 'ChaChenDetails.html')
+
+def ChaKolDetails(request):
+    return render(request, 'ChaKolDetails.html')
+
+def ChaMumbDetails1(request):
+    return render(request, 'ChaMumbDetails1.html')
+
+def ChaMumbDetails2(request):
+    return render(request, 'ChaMumbDetails2.html')
+
+def CulBangDetails1(request):
+    return render(request, 'CulBangDetails1.html')
+
+def CulBangDetails2(request):
+    return render(request, 'CulBangDetails2.html')
+
+def CulChenDetails(request):
+    return render(request, 'CulChenDetails.html')
+
+def CulKolDetails1(request):
+    return render(request, 'CulKolDetails1.html')
+
+def CulKolDetails2(request):
+    return render(request, 'CulKolDetails2.html')
+
+def CulMumbDetails(request):
+    return render(request, 'CulMumbDetails.html')
